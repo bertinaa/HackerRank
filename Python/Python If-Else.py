b8d544bac42471d8882ab83a94df9cee73d42dda
Bertina S <76837815+bertinaa@users.noreply.github.com>
2021-09-22 19:23:13 +0530

    Update Python If-Else.py

--- Python/Python If-Else.py
+++ Python/Python If-Else.py
@@ -10,3 +10,12 @@ import sys
 
 if __name__ == '__main__':
     n = int(raw_input().strip())
+if n%2==1:
+    print("Weird")
+elif n%2==0:
+    if n>=2 and n<=5:
+        print("Not Weird")
+    if n>=6 and n<=20:
+        print("Weird")
+    if n>20:
+        print("Not Weird")
