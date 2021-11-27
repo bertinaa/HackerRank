1466edf2f42c4cbd9792b6128d098dba67e459fb
Bertina S <76837815+bertinaa@users.noreply.github.com>
2021-11-27 11:28:36 +0530

    Update Java Stdin and Stdout II.java

--- Java/Java Stdin and Stdout II.java
+++ Java/Java Stdin and Stdout II.java
@@ -8,7 +8,14 @@ public class Solution {
         double d = scan.nextDouble();
         scan.nextLine();
         String s = scan.nextLine();
-    
+        /*
+        
+         If you use the nextLine() method immediately following the nextInt() method, 
+         recall that nextInt() reads integer tokens; because of this, 
+         the last newline character for that line of integer input is still queued in the input buffer 
+         and the next nextLine() will be reading the remainder of the integer line (which is empty).
+
+        */
         
         
         System.out.println("String: " + s);
