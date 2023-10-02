172cf14ec0414e235988a13b2c6fe320bb833894
Bertina S <76837815+bertinaa@users.noreply.github.com>
2023-10-02 13:43:09 +0530

    Update weather_III.sql

--- sql/weather_III.sql
+++ sql/weather_III.sql
@@ -1,5 +1,6 @@
 /*Query a list of CITY names from STATION for cities that have an even ID number. 
 Print the results in any order, but exclude duplicates from the answer.
+Also note, it's not ID%2==0 But ID%2=0
 */
 SELECT DISTINCT CITY
 FROM STATION
