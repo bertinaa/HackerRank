633c72689874736a15b6a1669a85d3aa5732c2cd
Bertina S <76837815+bertinaa@users.noreply.github.com>
2023-10-03 10:38:27 +0530

    Update weather_VI.sql

--- sql/weather_VI.sql
+++ sql/weather_VI.sql
@@ -1,7 +1,7 @@
 /*
-Enter your query here.
+Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
 */
+
 SELECT DISTINCT CITY
 FROM STATION
-WHERE CITY REGEXP '^[AEIOU]'
-ORDER BY CITY;
+WHERE CITY REGEXP '^[aeiouAEIOU]';
