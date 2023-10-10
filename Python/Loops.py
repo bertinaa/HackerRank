6be692c9ec1745fa8238117a9d7ff1a21b7f0be7
Bertina S <76837815+bertinaa@users.noreply.github.com>
2023-10-10 10:07:06 +0530

    Update Loops.py

--- Python/Loops.py
+++ Python/Loops.py
@@ -1,5 +1,16 @@
+"""
+Example
+n=3
+The list of non-negative integers that are less than n=3 is 0,1,2. 
+Print the square of each number on a separate line.
+
+o/p
+0
+1
+4
+"""
 if __name__ == '__main__':
-    n = int(raw_input())
+    n = int(input())
     for i in range(n):
         print(i**2)
         
